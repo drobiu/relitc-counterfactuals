9251eaf3f28f02ede50bf3921debd3f3b075be9e
Karol Dobiczek <k.t.dobiczek@student.tudelft.nl>
2024-01-22 20:34:37 +0100

    change config python version

--- scripts/extract_feature_importance.sh
+++ scripts/extract_feature_importance.sh
@@ -3,7 +3,7 @@ MAX_INPUT_LENGTH=256
 source $1    # Shareed params
 source $2    # Task and dataset args
 
-CUDA_VISIBLE_DEVICES=0 python extract_feature_importances.py \
+CUDA_VISIBLE_DEVICES=0 python3.9 extract_feature_importances.py \
         --data-path ${INPUT_DATA_PATH} \
         --model-path ${INPUT_MODELS_PATH} \
         --output-path ${DATA_PATH} \
